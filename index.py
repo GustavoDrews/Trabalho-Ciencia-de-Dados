b062ddb72436166810b98c068181aa24fc00560e
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-11-23 21:21:01 -0300

    new getCountries

--- index.py
+++ index.py
@@ -4,3 +4,12 @@ def getDados():
     arq = (open('covid.csv'))
     dados = csv.reader(arq)
     return(dados) 
+
+def getCountries():
+    dados = getDados()
+
+    countries = []
+    for item in dados:
+        if not item[1] in countries:
+            countries.append(item[1])
+    return(countries)
