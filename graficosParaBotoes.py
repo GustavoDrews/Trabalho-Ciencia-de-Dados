2ff972fff1ad80ec22e8e3138fb29ef973419c54
ViniMattos <vini.mattos.panambi@Gmail.com>
2023-11-30 20:56:36 -0300

    Chamando Mapa de Países na Tela Inicial

--- graficosParaBotoes.py
+++ graficosParaBotoes.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import index
+from matplotlib.colors import LinearSegmentedColormap
 
 def casosDiariosPaises(paisSelecionado):
     caminho_arquivo = 'covid.csv'
@@ -34,3 +36,78 @@ def casosDiariosPaises(paisSelecionado):
 
             # salva os graficos em uma pasta img
             # plt.savefig('img/'+country+'-grafico.png')
+
+
+def mortesDiariasPaises(paisSelecionado):
+    caminho_arquivo = 'covid.csv'
+
+    countriesdados = index.getCountries()
+
+    for country in countriesdados:
+        if country == paisSelecionado:
+            dados = pd.read_csv(caminho_arquivo)
+
+            print(dados.head())
+
+            pais = dados[dados['country'] == country]
+
+            pais['date'] = pd.to_datetime(pais['date'])
+            
+            # Cria o gráfico
+            plt.figure(figsize=(10, 6))
+            plt.bar(pais['date'], pais['daily_new_deaths'], color='red', alpha=0.7)
+
+            # Adiciona rótulos e título
+            plt.xlabel('Data')
+            plt.ylabel('Mortes diarias')
+            plt.title('Novas Mortes Diarias no '+ country +' ao longo do tempo')
+
+            # Rotaciona as datas para melhor legibilidade
+            plt.xticks(rotation=45)
+
+            # Exibe o gráfico
+            plt.show()
+
+            # salva os graficos em uma pasta img
+            # plt.savefig('img/'+country+'-grafico.png')
+
+def mapaPaises(info):
+    # Carrega os dados do arquivo CSV
+    caminho_arquivo = 'covid.csv'
+    dados = pd.read_csv(caminho_arquivo)
+
+    if info == 'mortes':
+        coluna = 'cumulative_total_deaths'
+    else:
+        coluna = 'cumulative_total_cases'
+
+    # Agrupa os dados por país e calcula o total acumulado de mortes ou casos
+    total = dados.groupby('country')[coluna].max().reset_index()
+
+    # Carrega um shapefile do mundo usando geopandas
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    # Merge dos dados do mundo com os dados do total
+    world = world.merge(total, how='left', left_on='name', right_on='country')
+
+    # Define os países com menor e maior
+    min = world[coluna].min()
+    max = world[coluna].max()
+
+    # Cria uma escala de cores personalizada de azul claro a vermelho forte
+    cmap = LinearSegmentedColormap.from_list('custom_map', ['lightblue', 'darkred'], N=256)
+    norm = plt.Normalize(vmin=min, vmax=max)
+
+    # Plota o mapa-múndi com a escala de cores personalizada
+    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+    world.boundary.plot(ax=ax)
+    if info == 'mortes':
+        world.plot(column=coluna, ax=ax, legend=True, legend_kwds={'label': 'Total de Mortes'}, cmap=cmap, norm=norm)
+
+        plt.title('Total de Mortes por País')
+    else:
+        world.plot(column=coluna, ax=ax, legend=True, legend_kwds={'label': 'Total de Casos'}, cmap=cmap, norm=norm)
+    
+        plt.title('Total de Casos por País')       
+    
+    plt.show()
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import tkinter as tk
 import graficosParaBotoes as gpbtn
 
-def grafico_por_pais():
+def grafico_casos_diarios():
     paisInformado = entry.get()
     gpbtn.casosDiariosPaises(paisInformado)
+
+def grafico_mortes_diarias():
+    paisInformado = entry.get()
+    gpbtn.mortesDiariasPaises(paisInformado)
+
+def grafico_mapa_mortes():
+    gpbtn.mapaPaises('mortes')
+def grafico_mapa_casos():
+    gpbtn.mapaPaises('casos')
+
 
 # Crie a janela principal
 window = tk.Tk()
@@ -16,9 +26,21 @@ window.geometry("400x200")
 entry = tk.Entry(window, width=30)
 entry.pack(side=tk.TOP, pady=20)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
 
-# Crie um botão para abrir uma nova janela
-open_button = tk.Button(window, text="Gráfico por País", command=grafico_por_pais)
-open_button.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+# Crie um botão para abrir uma nova janela com casos diários
+open_button_casos_diarios = tk.Button(window, text="Casos Diários por País", command=grafico_casos_diarios)
+open_button_casos_diarios.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+
+# Crie um botão para abrir uma nova janela com mortes diárias
+open_button_mortes_diarias = tk.Button(window, text="Mortes Diárias por País", command=grafico_mortes_diarias)
+open_button_mortes_diarias.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+
+# Crie um botão para abrir uma nova janela com o mapa de mortes
+open_button_mapa_mortes = tk.Button(window, text="Mapa de Mortes", command=grafico_mapa_mortes)
+open_button_mapa_mortes.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+
+# Crie um botão para abrir uma nova janela com o mapa de mortes
+open_button_mapa_casos = tk.Button(window, text="Mapa de Casos", command=grafico_mapa_casos)
+open_button_mapa_casos.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
 
 # Execute o loop de eventos do Tkinter
 window.mainloop()
