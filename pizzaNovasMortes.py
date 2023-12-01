8ee9ec9240bced45346cd23a097808846367ca65
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2023-11-30 21:19:41 -0300

    busca o top "valor" selecionado pelo usuario

--- pizzaNovasMortes.py
+++ pizzaNovasMortes.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def getGraficoPizza():
+def getGraficoPizza(quant):
     dados = pd.read_csv("covid.csv")
 
-    cases_by_country = dados.groupby('country')['daily_new_cases'].sum().head(5) 
+    cases_by_country = dados.groupby('country')['daily_new_cases'].sum() 
 
-    df = pd.DataFrame({'country': cases_by_country.index, 'daily_new_cases': cases_by_country.values})
+    df = pd.DataFrame({'country': cases_by_country.index, 'Novos casos diários': cases_by_country.values})
 
-    df.plot.pie(y='daily_new_cases', labels=df['country'], autopct='%1.1f%%', startangle=90, legend=True)
+    df = df.sort_values(by='Novos casos diários', ascending=False).head(quant)
 
-    plt.title('Novos casos Diarios (Covid-19)')
+    df.plot.pie(y='Novos casos diários', labels=df['country'], autopct='%1.1f%%', startangle=90, legend=False)
+
+    plt.title('TOP '+str(quant)+' Novos casos Diários (Covid-19)')
 
     plt.show()
