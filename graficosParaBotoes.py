bcf61075bd97b7e9691f8d424a843319f8bfc1c7
ViniMattos <vini.mattos.panambi@Gmail.com>
2023-11-30 21:43:19 -0300

    Versão Final 1

--- graficosParaBotoes.py
+++ graficosParaBotoes.py
@@ -26,7 +26,7 @@ def casosDiariosPaises(paisSelecionado):
             # Adiciona rótulos e título
             plt.xlabel('Data')
             plt.ylabel('Novos Casos Diarios')
-            plt.title('Novos Casos Diarios no '+ country +' ao longo do tempo')
+            plt.title('Novos Casos Diarios em '+ country +' ao longo do tempo')
 
             # Rotaciona as datas para melhor legibilidade
             plt.xticks(rotation=45)
@@ -60,7 +60,7 @@ def mortesDiariasPaises(paisSelecionado):
             # Adiciona rótulos e título
             plt.xlabel('Data')
             plt.ylabel('Mortes diarias')
-            plt.title('Novas Mortes Diarias no '+ country +' ao longo do tempo')
+            plt.title('Novas Mortes Diarias em '+ country +' ao longo do tempo')
 
             # Rotaciona as datas para melhor legibilidade
             plt.xticks(rotation=45)
@@ -110,4 +110,34 @@ def mapaPaises(info):
     
         plt.title('Total de Casos por País')       
     
+    plt.show()
+
+def pizzaNovosCasos(quant):
+    dados = pd.read_csv("covid.csv")
+
+    cases_by_country = dados.groupby('country')['daily_new_cases'].sum() 
+
+    df = pd.DataFrame({'country': cases_by_country.index, 'Novos casos diários': cases_by_country.values})
+
+    df = df.sort_values(by='Novos casos diários', ascending=False).head(quant)
+
+    df.plot.pie(y='Novos casos diários', labels=df['country'], autopct='%1.1f%%', startangle=90, legend=False)
+
+    plt.title('TOP '+str(quant)+' Novos casos Diários (Covid-19)')
+
+    plt.show()
+
+def pizzaNovasMortes(quant):
+    dados = pd.read_csv("covid.csv")
+
+    cases_by_country = dados.groupby('country')['daily_new_deaths'].sum() 
+
+    df = pd.DataFrame({'country': cases_by_country.index, 'Novas mortes diárias': cases_by_country.values})
+
+    df = df.sort_values(by='Novas mortes diárias', ascending=False).head(quant)
+
+    df.plot.pie(y='Novas mortes diárias', labels=df['country'], autopct='%1.1f%%', startangle=90, legend=False)
+
+    plt.title('TOP '+str(quant)+' Novas mortes Diárias (Covid-19)')
+
     plt.show()
--- main.py
+++ main.py
@@ -3,16 +3,50 @@ import graficosParaBotoes as gpbtn
 
 def grafico_casos_diarios():
     paisInformado = entry.get()
-    gpbtn.casosDiariosPaises(paisInformado)
+
+    if paisInformado == '':
+        print('Informe um país válido (em inglês) no campo texto')
+    else:
+        gpbtn.casosDiariosPaises(paisInformado)
 
 def grafico_mortes_diarias():
     paisInformado = entry.get()
-    gpbtn.mortesDiariasPaises(paisInformado)
+
+    if paisInformado == '':
+        print('Informe um país válido (em inglês) no campo texto')
+    else:
+        gpbtn.mortesDiariasPaises(paisInformado)
 
 def grafico_mapa_mortes():
     gpbtn.mapaPaises('mortes')
+
 def grafico_mapa_casos():
     gpbtn.mapaPaises('casos')
+
+def grafico_pizza_casos():
+    numeroPaisesInformado = entry.get()
+    if numeroPaisesInformado == '':
+        numeroPaisesInformado = 0
+    else:
+        numeroPaisesInformado = int(numeroPaisesInformado)
+
+    if numeroPaisesInformado <= 0:
+        print('É necessário informar o número de países a serem listados no campo')
+    else:   
+        gpbtn.pizzaNovosCasos(numeroPaisesInformado)
+
+def grafico_pizza_mortes():    
+    numeroPaisesInformado = entry.get()
+
+    if numeroPaisesInformado == '':
+        numeroPaisesInformado = 0
+    else:
+        numeroPaisesInformado = int(numeroPaisesInformado)
+
+    if numeroPaisesInformado <= 0:
+        print('É necessário informar o número de países a serem listados no campo')
+    else:  
+        gpbtn.pizzaNovasMortes(numeroPaisesInformado)
 
 
 # Crie a janela principal
@@ -41,6 +75,14 @@ open_button_mapa_mortes.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinh
 # Crie um botão para abrir uma nova janela com o mapa de mortes
 open_button_mapa_casos = tk.Button(window, text="Mapa de Casos", command=grafico_mapa_casos)
 open_button_mapa_casos.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+
+# Crie um botão para abrir uma nova janela com o mapa de mortes
+open_button_pizza_casos = tk.Button(window, text="Gráfico de Pizza de Casos", command=grafico_pizza_casos)
+open_button_pizza_casos.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
+
+# Crie um botão para abrir uma nova janela com o mapa de mortes
+open_button_pizza_mortes = tk.Button(window, text="Gráfico de Pizza de Mortes", command=grafico_pizza_mortes)
+open_button_pizza_mortes.pack(side=tk.TOP, pady=10)  # Use side=tk.TOP para alinhar ao topo e adicione pady para espaçamento
 
 # Execute o loop de eventos do Tkinter
 window.mainloop()
